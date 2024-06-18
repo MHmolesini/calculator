85de686a1bdc395223c48a2b84283e3f328947f4
Matias Molesini <Mhmolesini@gmail.com>
2024-06-17 22:08:30 -0300

    estilos a la calculadora y botones

---
+++ src/components/Boton.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./boton.css";
+
+const Boton = (params) => {
+  const { texto, clase } = params;
+
+  return <button className={clase}>{texto}</button>;
+};
+
+export default Boton;
